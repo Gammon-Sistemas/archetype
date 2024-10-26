7074492fb90dca9600ee3849adc2a10ddd1b6760
Hugo Henrique <hugo.hbs64@gmail.com>
2024-10-26 18:38:46 -0300

    First commit

---
+++ src/main/java/com/archetypePostgres/ArchetypePostgresApplication.java
@@ -0,0 +1,13 @@
+package com.archetypePostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchetypePostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchetypePostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archetypePostgres/ArchetypePostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archetypePostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchetypePostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
